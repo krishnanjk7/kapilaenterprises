cdd93cbbc3260fce59b096094b8f4c624f7287ff
Jayakrishnan <d.jayakrishnan@lakeba.com>
2020-06-27 14:31:50 +0530

    products added

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -7,6 +7,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductsComponent implements OnInit {
 
+  products = [
+    {
+      name: 'Turbo Ventilator',
+      description: `This Aluminum Roof Ventilator is ensured for dependable execution Decode from g ertatumim as has 13 the
+      heaviness of steel and in this way the inc reded to move the ventilator from array Dict Is a great deal
+      less.`,
+      image: 'turbo-ventilator.jpg'
+    },
+    {
+      name: 'Aluminium Foil (Polynum roll)',
+      description: `The energy crisis globally has brought about an increase in combustible costs, where only an appropriate insulation system can bring about significant economic savings. The need to insulate buildings at all exposed or susceptible surfaces from inside or outside is an indisputable reality that finally goes on to increasing the comfort of occupants both temporary and permanent, humans and livestock and finally products and good that are heat and moisture sensitive`,
+      image: 'aluminium-foil.jpg'
+    },
+    {
+      name: 'Polycarbonate Sheet',
+      description: '',
+      image: 'polycarbonate-sheet.jpeg'
+    },
+    {
+      name: 'Self Drilling Screws',
+      description: '',
+      image: 'self-drilling-screws.jpg'
+    },
+    {
+      name: 'Butyle Tape',
+      description: '',
+      image: 'butyle-tape.jpg'
+    },
+    {
+      name: 'Sealant',
+      description: '',
+      image: 'Sealant.png'
+    },
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
